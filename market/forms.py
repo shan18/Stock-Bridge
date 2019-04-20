77808b203197ebcfcba17a0aa86633cb03164201
Shantanu <thegeek.004@gmail.com>
2019-04-20 21:45:48 +0530

    Update transaction view

--- market/forms.py
+++ market/forms.py
@@ -2,19 +2,6 @@ from django import forms
 from .models import TRANSACTION_MODES
 
 
-class StockTransactionForm(forms.Form):
-    mode = forms.ChoiceField(choices=TRANSACTION_MODES)
-    quantity = forms.CharField(
-        required=True,
-        widget=forms.TextInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'Enter integers only',
-            'title': 'Enter integers only',
-            'pattern': '[0-9]+'
-            })
-    )
-
-
 class CompanyChangeForm(forms.Form):
     price = forms.CharField(required=True, widget=forms.TextInput(attrs={
         'class': 'form-control',
@@ -22,5 +9,3 @@ class CompanyChangeForm(forms.Form):
         'title': 'Enter integers only',
         'placeholder': 'Enter integers only'
     }))
-
-
