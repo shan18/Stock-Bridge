c7c02740b977160a70bd50fccfa3d1f873b01aae
Shantanu <thegeek.004@gmail.com>
2019-05-06 13:22:58 +0530

    add production settings

--- stock_bridge/settings/base.py
+++ stock_bridge/settings/base.py
@@ -5,12 +5,11 @@ from decouple import config
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'localhost']
@@ -159,15 +158,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn', 'static_root')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static_cdn', 'media_root')
-
-
-# removing SSL/TLS settings for local environment
-CORS_REPLACE_HTTPS_REFERER = False
-HOST_SCHEME = "http://"
-SECURE_PROXY_SSL_HEADER = None
-SECURE_SSL_REDIRECT = False
-SESSION_COOKIE_SECURE = False
-CSRF_COOKIE_SECURE = False
-SECURE_HSTS_SECONDS = None
-SECURE_HSTS_INCLUDE_SUBDOMAINS = False
-SECURE_FRAME_DENY = False
---
+++ stock_bridge/settings/production.py
@@ -0,0 +1,22 @@
+from .base import *
+from datetime import datetime
+
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['']
+BASE_URL = ''
+
+START_TIME = datetime(2019, 5, 12, 19, 00, 0)
+STOP_TIME = datetime(2019, 5, 12, 23, 59, 59)
+
+# SSL/TLS settings for https security
+CORS_REPLACE_HTTPS_REFERER = True
+HOST_SCHEME = "https://"
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_SECONDS = 1000000
+SECURE_FRAME_DENY = True
