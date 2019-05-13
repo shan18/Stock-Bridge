1cc57f4a9cfa10ecede89130693906c5b2a7b9cc
Shantanu <thegeek.004@gmail.com>
2019-05-14 00:02:48 +0530

    update start time

--- stock_bridge/settings/production.py
+++ stock_bridge/settings/production.py
@@ -8,8 +8,8 @@ DEBUG = False
 ALLOWED_HOSTS = ['stockbridge.morphosis.org.in', '.herokuapp.com']
 BASE_URL = 'stockbridge.morphosis.org.in'
 
-START_TIME = datetime(2019, 5, 17, 19, 00, 0)
-STOP_TIME = datetime(2019, 5, 18, 23, 59, 59)
+START_TIME = datetime(2019, 5, 14, 19, 00, 0)
+STOP_TIME = datetime(2019, 5, 15, 00, 30, 00)
 
 DATABASES = {
     'default': dj_database_url.config(
