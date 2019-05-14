c3f061fa48f85069a26b4d9b3e0454735faac846
Shantanu <thegeek.004@gmail.com>
2019-05-14 19:24:40 +0530

    fasd

--- session/middleware.py
+++ session/middleware.py
@@ -11,17 +11,17 @@ class OneSessionPerUserMiddleware:
         """ Code to be executed for each request before
             the views (and later middleware) are called.
         """
-        if request.user.is_authenticated:
-            stored_session_key = request.user.loggedinuser.session_key
+        # if request.user.is_authenticated:
+        #     stored_session_key = request.user.loggedinuser.session_key
 
-            # If there is a stored_session_key in the database and it is
-            # different from the current session key then delete the session
-            # with the stored_session_key from the Session table
-            if stored_session_key and stored_session_key != request.session.session_key:
-                Session.objects.get(session_key=stored_session_key).delete()
+        #     # If there is a stored_session_key in the database and it is
+        #     # different from the current session key then delete the session
+        #     # with the stored_session_key from the Session table
+        #     if stored_session_key and stored_session_key != request.session.session_key:
+        #         Session.objects.get(session_key=stored_session_key).delete()
 
-            request.user.loggedinuser.session_key = request.session.session_key
-            request.user.loggedinuser.save()
+        #     request.user.loggedinuser.session_key = request.session.session_key
+        #     request.user.loggedinuser.save()
         
         response = self.get_response(request)
         return response
